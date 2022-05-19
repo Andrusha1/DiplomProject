26a62abc03727bd4fe6bc62629620e3e192ea633
Andrew Ryzhov <taaaaaaaag@yandex.ru>
2022-05-20 02:46:17 +0400

    Added page "Add new street"

--- DiplomProject.Data/Street.cs
+++ DiplomProject.Data/Street.cs
@@ -14,9 +14,9 @@ namespace DiplomProject.Data
 	public class Area
 	{
 		public long id { get; set; }
-		public int placesAmount { get; set; } //Количество парковочных мест
+		public int placesAmount { get; set; } //Стоимость аренды одного парковочного места
 		public int rentPrice { get; set; }
-		public int parkingPlaces { get; set; }
+		public int parkingPlaces { get; set; } //Количество парковочных мест
 	}
 }
 
---
+++ DiplomProject/Controllers/AddStreetButtonController.cs
@@ -0,0 +1,36 @@
+﻿using DiplomProject.Data;
+using DiplomProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomProject.Controllers
+{
+    public class AddStreetButtonController : Controller
+    {
+        private readonly DatabaseContext context;
+        public AddStreetButtonController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddStreet(string name, int numberOfHouses, string typeOfHouses, int placesAmount, int rentPrice, int parkingPlaces)
+        {
+            context.streets.Add(new Street
+            {
+                name = name,
+                numbersOfHouses = numberOfHouses,
+                typeOfHouses = typeOfHouses,
+                areas = new List<Area>
+                {
+                     new Area{parkingPlaces = parkingPlaces, rentPrice = rentPrice, placesAmount = placesAmount},
+                }
+            });
+            
+            return View();
+        }
+    }
+}
